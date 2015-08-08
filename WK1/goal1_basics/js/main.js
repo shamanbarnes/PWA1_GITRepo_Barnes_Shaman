d86a4d9fe1278e0bf5333eee4739afc0220d6798
Shaman Barnes <shamanbarnes1@me.com>
2015-08-08 13:08:37 -0400

    fight function

--- WK1/goal1_basics/js/main.js
+++ WK1/goal1_basics/js/main.js
@@ -25,3 +25,15 @@
     var playerTwoHealth = 100;
 
     var round = 0;
+
+    function fight(){
+        console.log("in the fight function");
+
+        alert(playerOneName+" : "+ playerOneHealth +" *START* " + playerTwoName + " : " + playerTwoHealth);
+
+        for (var i=0; i<10; i++){
+
+            var minDamage1 = player1Damage * .5;
+            var minDamage2 = player2Damage * .5;
+            var f1 = Math.floor(Math.random() * (player1Damage - minDamage1) + minDamage1);
+            var f2 = Math.floor(Math.random() * (player2Damage - minDamage2) + minDamage2);
