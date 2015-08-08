d4b58ebba76850ec25b4135a1786d92998561fd7
Shaman Barnes <shamanbarnes1@me.com>
2015-08-08 13:06:25 -0400

    self-execution function

--- WK1/goal1_basics/js/main.js
+++ WK1/goal1_basics/js/main.js
@@ -2,3 +2,18 @@
     Date: 8/6/15
     Assignment: Goal1: Duell
 */
+
+
+//Self-execution function 
+
+(function(){
+
+    console.log("Fight!!!");
+
+    //player name
+
+    var playerOneName = "Spiderman";
+    var playerTwoName = "Batman";
+
+    //player damage
+
